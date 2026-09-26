d7cd1a3a63478702482da4ad1b3c60508111b37d
463214047 <463214047@qq.com>
2026-09-26 10:13:05 +0800

    refactor(layout): 移除未使用的字体样式及设置中文语言
    移除了未使用的 Geist 字体导入和全局样式文件，同时将页面默认语言设置为简体中文

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,4 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 
 export const metadata: Metadata = {
   title: {
@@ -8,23 +6,11 @@ export const metadata: Metadata = {
     template: "%s - Ayaka",
   },
 };
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang="zh-CN">
+      <body>{children}</body>
     </html>
   );
 }
